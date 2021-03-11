aab0c0adcc075bd427a8be498a71570995e9aefa
Martin Reynolds <martinandtoni@btinternet.com>
2021-03-11 23:49:00 +0000

    add Function and Display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,17 @@
+// Write a simple program to combine a first name and a last name inside a function.
+// Then update the function to accept a first and last name as arguments.
+
+// Add a return statement to your 'name' function. Use that function to set the value of a variable.
+
+function displayFullName(fName, lName) {
+  var fName = document.getElementById("fName").value;
+  var lName = document.getElementById("lName").value;
+
+  return (document.getElementById(
+    "fullNameDisplay"
+  ).innerHTML = `${fName} ${lName}`);
+}
+
 // Write a function that outputs a sentence. Then invoke that function later in your code.
 
 function createSentence() {
