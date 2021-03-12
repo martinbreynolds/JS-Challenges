ad89fc3789065c08b92ba1aba914fedf9aacfc2e
Martin Reynolds <martinandtoni@btinternet.com>
2021-03-12 00:20:49 +0000

    add Function and Display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,8 +3,6 @@
 // Part 1
 // Make a variable called temperature. Write some code that tells you to put on a coat if it is below 50 degrees.
 
-// Part 2
-//  If it's less than 50 degrees, wear a coat. If it's less than 30 degrees, wear a coat and a  hat. If it's less than 0 degrees, stay inside. Otherwise, just pants and vest is fine.
 function coatCheck() {
   var temperature;
   var coatWeather;
@@ -17,6 +15,31 @@ function coatCheck() {
   temp = parseInt(temperature);
   coatWeather = temp < 50 ? "Put on your coat!" : "No coat required!";
   document.getElementById("coatAdvice").innerHTML = coatWeather;
+}
+
+// Part 2
+//  If it's less than 50 degrees, wear a coat.
+// If it's less than 30 degrees, wear a coat and a  hat.
+// If it's less than 0 degrees, stay inside.
+// Otherwise, just pants and vest is fine.
+
+function coatCheck2() {
+  var temperature2;
+  var coatWeather2;
+
+  temperature2 = document.getElementById("temperature2").value;
+  if (temperature2 == "") {
+    return;
+  }
+
+  temp2 = parseInt(temperature2);
+
+  if (temp2 >= 50) coatWeather2 = "Pants and Vest are fine";
+  else if (temp2 >= 30) coatWeather2 = "Wear a Coat";
+  else if (temp2 >= 0) coatWeather2 = "Wear a Coat and a Hat";
+  else coatWeather2 = "Stay Inside";
+
+  document.getElementById("coatAdvice2").innerHTML = coatWeather2;
 }
 
 //Full Name Display
