f0b6e2ecbd3a18e5ce84c05ee22f272f1245d6cf
Martin Reynolds <martinandtoni@btinternet.com>
2021-03-12 15:28:08 +0000

    Add Scripts and Display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -103,3 +103,18 @@ function fortuneTeller(numberOfChildren, partnersName, location, jobTitle) {
     "yourFortune"
   ).innerHTML = `You will be a ${jobTitle} in ${location}, and married to ${partnersName} with ${numberOfChildren} kids.`);
 }
+
+//Percentage Calculator
+
+//Create a percentage calculator that takes a number and % and return the percentage value.
+
+function percentageCalculator(num, per) {
+  var num = parseInt(
+    document.getElementById("percentageCalculatorNumber").value
+  );
+  var per = parseInt(
+    document.getElementById("percentageCalculatorPercent").value
+  );
+  return (document.getElementById("percentageResult").innerHTML =
+    (num / 100) * per);
+}
