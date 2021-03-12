87c0559f31739f9b5ee3ca44169af24a28cd5cd2
Martin Reynolds <martinandtoni@btinternet.com>
2021-03-12 17:38:49 +0000

    add script and display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -155,3 +155,48 @@ function outputDrinksOrder(size, drink) {
       break;
   }
 }
+
+//Calculator
+
+//Build a simple calculator that takes two integers and the operator and displays the result.
+
+function calculator(num1, num2, operator) {
+  var num1 = parseInt(document.getElementById("num1").value);
+  var num2 = parseInt(document.getElementById("num2").value);
+  var operator = document.getElementById("operator").value;
+  switch (operator) {
+    case "+":
+      var result = num1 + num2;
+      document.getElementById(
+        "calculatorResult"
+      ).innerHTML = `${num1} ${operator} ${num2} = ${result}`;
+      break;
+    case "-":
+      var result = num1 - num2;
+      document.getElementById(
+        "calculatorResult"
+      ).innerHTML = `${num1} ${operator} ${num2} = ${result}`;
+      return;
+      break;
+    case "*":
+      var result = num1 * num2;
+      document.getElementById(
+        "calculatorResult"
+      ).innerHTML = `${num1} ${operator} ${num2} = ${result}`;
+      return;
+      break;
+    case "/":
+      var result = num1 / num2;
+      document.getElementById(
+        "calculatorResult"
+      ).innerHTML = `${num1} ${operator} ${num2} = ${result}`;
+      return;
+      break;
+    case "%":
+      var result = num1 % num2;
+      document.getElementById(
+        "calculatorResult"
+      ).innerHTML = `${num1} ${operator} ${num2} = ${result}`;
+      return;
+  }
+}
